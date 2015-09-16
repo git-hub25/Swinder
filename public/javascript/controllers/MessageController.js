afd8de0efb20730158041210de0239556bc646b5
kandianne <kandipierre@gmail.com>
2015-09-15 20:19:54 -0700

    Added Controllers & linked to Index

---
+++ public/javascript/controllers/MessageController.js
@@ -0,0 +1,12 @@
+(function() {
+	'use strict';
+	angular.module('app')
+	.controller('MessageController', MessageController);
+
+	MessageController.$inject = [];
+
+	function MessageController() {
+		var vm = this;
+
+	}
+})();
---
+++ public/javascript/controllers/NavBarController.js
@@ -0,0 +1,12 @@
+(function() {
+	'use strict';
+	angular.module('app')
+	.controller('NavBarController', NavBarController);
+
+	NavBarController.$inject = [];
+
+	function NavBarController() {
+		var vm = this;
+
+	}
+})();
