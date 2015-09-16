d1041e8c96747642b77dc19d65c8c176582f3c6f
kandianne <kandipierre@gmail.com>
2015-09-15 22:21:01 -0700

    added html views

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -6,8 +6,31 @@
 	function Config($stateProvider, $urlRouterProvider) {
 		$stateProvider.state('Home',{
 			url: '/',
-			templateUrl: 'views/home.html'
+			templateUrl: 'views/home.html',
+			controller: "HomeController",
+			controllerAs: "vm"
+		}).state('RegisterCouple',{
+			url: '/RegisterCouple',
+			templateUrl: 'views/register_couple.html',
+			controller: "NavBarController",
+			controllerAs: "vm"
+		}).state('Profile',{
+			url: '/Profile',
+			templateUrl: 'views/profile.html',
+			controller: "ProfileController",
+			controllerAs: "vm"
+		}).state('CreateMessage',{
+			url: '/CreateMessage',
+			templateUrl: 'views/create_message.html',
+			controller: "MessageController",
+			controllerAs: "vm"
+		}).state('EditProfile',{
+			url: '/EditProfile',
+			templateUrl: 'views/edit_profile.html',
+			controller: "ProfileController",
+			controllerAs: "vm"
 		});
+
 		$urlRouterProvider.otherwise('/');
 	}
 })();
