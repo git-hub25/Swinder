3ec3bc4087ead67a1b929ba9134e750f30341f66
lentusignavus <ignavus.ii@gmail.com>
2015-09-16 00:17:28 -0700

    Password hashing functionality fixed. Minor naming bug

--- routes/CoupleRoutes.js
+++ routes/CoupleRoutes.js
@@ -9,75 +9,110 @@ var Salts = mongoose.model("Salts");
 
 //register
 router.post('/register', function(req, res) {
-	var couple = new Couple(req.body);
-	//Save couple with passwordHash field blank
-	couple.save(function(err, couple) {
-		console.log("couples line17");
-		if(err) console.log(err);
-		if(err) return res.status(500).send({err: "Issues with Swinder's server :/"});
-		if(!couple) return res.status(400).send({err: 'You messed up!'});
-		console.log("couples line21");
-				//Creates salt with id from new couple as reference
-				var salt = new Salts({coupleId: couple._id});
-		//Sets salt string with SaltSchema method on Salts.js with crypto 64 bytes
-		salt.setSalt();
-		//Saves salt
-		salt.save(function(err, salt) {
-			if(err) return res.status(500).send({err: "Issues with Swinder's server :/"});
-			if(!salt) return res.status(400).send({err: 'You messed up!'});
-			console.log("couples line28");
-			//Update scouple with reference to salt with salt._id
-			Couple.update({_id: salt.coupleId}, {salt: salt._id}, function(err, coupleWithSaltId) {
-				if(err) return res.status(500).send({err: "Issues with Swinder's server :/"});
-				if(!coupleWithSaltId) return res.status(400).send({err: 'You messed up!'});
-				console.log("couples line32");
-				//Finds couple to populate salt string to change reference id of salt to actual salt string
-				Couple.find({_id: coupleWithSaltId._id})
-				.populate({
-					path: "salt",
-					model: "Salts",
-					select: "salt"
-				})
-				.exec(function(err, coupleWithSaltString) {
-					if(err) return res.status(500).send({err: "Issues with Swinder's server :/"});
-					if(!coupleWithSaltString) return res.status(400).send({err: 'You messed up!'});
-					console.log("couples line42");
-					console.log(coupleWithSaltString);
-									//Hashes the password with salt with Couple setPassword method defined in Couple.js
-					// coupleWithSaltString.setPassword(req.body.password, coupleWithSaltString.salt)
-					res.send()
-					
-				});
-			});
-});
-});
+  var couple = new Couple(req.body);
+  //Save couple with passwordHash field blank
+  couple.save(function(err, couple) {
+    console.log("couples line17");
+    if (err) console.log(err);
+    if (err) return res.status(500).send({
+      err: "Issues with Swinder's server :/"
+    });
+    if (!couple) return res.status(400).send({
+      err: 'You messed up!'
+    });
+    //Creates salt with id from new couple as reference
+    var salt = new Salts({
+      coupleId: couple._id
+    });
+    //Sets salt string with SaltSchema method on Salts.js with crypto 64 bytes
+    salt.setSalt();
+    //Saves salt
+    salt.save(function(err, salt) {
+      if (err) return res.status(500).send({
+        err: "Issues with Swinder's server :/"
+      });
+      if (!salt) return res.status(400).send({
+        err: 'You messed up!'
+      });
+      //Update scouple with reference to salt with salt._id
+      Couple.update({
+        _id: salt.coupleId
+      }, {
+        salt: salt._id
+      }, function(err, coupleWithSaltId) {
+        if (err) return res.status(500).send({
+          err: "Issues with Swinder's server :/"
+        });
+        if (!coupleWithSaltId) return res.status(400).send({
+          err: 'You messed up!'
+        });
+        //Finds couple to populate salt string to change reference id of salt to actual salt object
+        console.log(coupleWithSaltId);
+        Couple.findOne({
+            _id: salt.coupleId
+          })
+          .populate({
+            path: "salt",
+            model: "Salts",
+            select: "salt"
+          })
+          .exec(function(err, coupleWithSaltString) {
+            if (err) return res.status(500).send({
+              err: "Issues with Swinder's server :/"
+            });
+            if (!coupleWithSaltString) return res.status(400).send({
+              err: 'You messed up!'
+            });
+            //Hashes the password with salt with Couple setPassword method defined in Couple.js
+            //Seconf param needs to be .salt.salt because salt is object with salt property that contains salt string from crypto
+            coupleWithSaltString.setPassword(coupleWithSaltString.passwordHash, coupleWithSaltString.salt.salt);
+            Couple.update({
+              _id: coupleWithSaltString._id
+            }, coupleWithSaltString, function(err, result) {
+              if (err) return res.status(500).send({
+                err: "Issues with Swinder's server :/"
+              });
+              if (!result) return res.status(400).send({
+                err: 'You messed up!'
+              });
+              res.send();
+            });
+          });
+      });
+    });
+  });
 });
 
 //local passport
 router.post('/login', function(req, res) {
-	passport.authenticate('local', function(err, couple, info) {
-		if(!couple) return res.status(400).send(info);
-		res.send({token: couple.generateJWT()});
-	})(req, res, next)
+  passport.authenticate('local', function(err, couple, info) {
+    if (!couple) return res.status(400).send(info);
+    res.send({
+      token: couple.generateJWT()
+    });
+  })(req, res, next)
 });
 
 //getting an individual couple
 router.param('id', function(req, res, next, id) {
-	shows.findcouple(req.params.id, function(err, couple) {
-		if(err) return next({err: err, type: 'client'});
-		req.couple = couple;
-		next();
-	});
+  shows.findcouple(req.params.id, function(err, couple) {
+    if (err) return next({
+      err: err,
+      type: 'client'
+    });
+    req.couple = couple;
+    next();
+  });
 });
 
 //GET /couples
 router.get('/couple', function(req, res) {
-	res.send() //do i need to add an array in as the parameter?
+  res.send() //do i need to add an array in as the parameter?
 });
 
 //GET /couple /{coupleId}
 router.get('/couple/:id', function(req, res) {
-	res.send(req.couple);
+  res.send(req.couple);
 });
 
 module.exports = router;
