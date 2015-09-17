a1f05fbe324f9774cb9a11ea6d1d6308351033ca
lentusignavus <ignavus.ii@gmail.com>
2015-09-16 20:35:46 -0700

    Fixed a few minor bugs

--- public/javascript/controllers/MainController.js
+++ public/javascript/controllers/MainController.js
@@ -1,41 +1,41 @@
 (function() {
-	'use strict';
-	angular.module('app')
-	.controller('MainController', MainController);
+  'use strict';
+  angular.module('app')
+    .controller('MainController', MainController);
 
-	MainController.$inject = ["CoupleFactory", "$rootScope", 'MessageFactory'];
+  MainController.$inject = ["CoupleFactory", "$rootScope", 'MessageFactory'];
 
-	function MainController(CoupleFactory, $rootScope, MessageFactory) {
-		var vm = this;
-		vm.title = 'Swinder';
-		vm.matches = [];
-		vm.getProfiles = function() {
-			CoupleFactory.getCouples().then(function(res) {
-				vm.getCouples = res;
-				vm.randomProfile = vm.profileRandomizer(vm.getCouples);
-				console.log(vm.getCouples);
-			});
-		};
+  function MainController(CoupleFactory, $rootScope, MessageFactory) {
+    var vm = this;
+    vm.title = 'Swinder';
+    vm.matches = [];
+
+    vm.profileRandomizer = function(array) {
+      var random = Math.floor((Math.random() * array.length));
+      console.log(random);
+      return array[random];
+    };
+
+    vm.getProfiles = function() {
+      CoupleFactory.getCouples().then(function(res) {
+        vm.getCouples = res;
+        vm.randomProfile = vm.profileRandomizer(vm.getCouples);
+        console.log(vm.getCouples);
+      });
+    };
 
 
-		// var loggedInCouple = $rootScope._couple ;
+    // var loggedInCouple = $rootScope._couple ;
 
-		}
-		// Testing getConversation
-		// vm.getConversation(26, 26) ;
 
-		vm.getProfiles();
+    // vm.getProfiles();
 
-		vm.profileRandomizer = function(array) {
-			var random = Math.floor((Math.random()*array.length));
-			console.log(random)
-			return array[random];
-		};
 
-		vm.likeProfile = function(profile) {
-			vm.matches.push(profile);
-			vm.getProfiles();
-		}
 
-	}
+    vm.likeProfile = function(profile) {
+      vm.matches.push(profile);
+      vm.getProfiles();
+    }
+  }
+
 })();
