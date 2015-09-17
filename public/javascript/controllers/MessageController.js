838c674048a709132288d6670c41b3e79f2960a8
Hamad Alhassen <halhassen@gmail.com>
2015-09-17 11:52:10 -0700

    message and conversation function working, needs displaying next

--- public/javascript/controllers/MessageController.js
+++ public/javascript/controllers/MessageController.js
@@ -1,38 +1,31 @@
 (function() {
-    'use strict';
-    angular.module('app')
-      .controller('MessageController', MessageController);
+  'use strict';
+  angular.module('app')
+  .controller('MessageController', MessageController);
 
-    MessageController.$inject = ['$state', 'MessageFactory', '$rootScope'];
+  MessageController.$inject = ['$state', 'MessageFactory', '$rootScope'];
 
-    function MessageController($state, MessageFactory, $rootScope) {
-      var vm = this;
-      vm.message = {};
+  function MessageController($state, MessageFactory, $rootScope) {
+    var vm = this;
+    vm.message = {};
 
-      vm.getMessages = function() {
-        MessageFactory.getMessages().then(function(res) {
-          vm.messages = res;
-          // console.log(res);
-        });
-      };
+    vm.loadConversations = function() {
+      MessageFactory.getConversation().then(function(res) {
+        vm.conversationMessages = res.message;
+        console.log(res);
+      });
+    }
 
-      vm.loadConversations = function() {
-        MessageFactory.getConversation().then(function(res) {
-          console.log(res);
-        });
-      }
-
-      vm.loadConversations();
+    vm.loadConversations();
 
 
       //not working what the fuck
       vm.sendMessage = function() {
         vm.message.createdDate = new Date();
-        vm.message.createdBy = vm.loggedInCouple.id;
+        vm.message.createdBy = $rootScope.id;
         vm.message.conversationId = $rootScope._conversation._id;
         MessageFactory.sendMessage(vm.message).then(function(res) {
           delete vm.message;
-          vm.getMessages();
         });
         console.log('vm.sendMessage!')
       };
@@ -51,4 +44,4 @@
 
       //vm.getMessages();
     }
-})();
+  })();
